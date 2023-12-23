4af13c30394154fea53906d05481c5f2ce5b6525
malekbizid <153116052+malekbizid@users.noreply.github.com>
2023-12-23 14:07:05 +0100

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,61 +1,77 @@
 from tabulate import tabulate
+from datetime import datetime
 
-# Fonction pour calculer les intérêts simples sur une plage d'années
-def calculer_interets_simples(capital, taux, debut_annee, fin_annee):
+# Fonction pour calculer les intérêts simples sur une plage de temps
+def calculer_interets_simples(capital, taux, debut_annee, debut_mois, fin_annee, fin_mois):
     data = []
-    for annee in range(debut_annee, fin_annee + 1):
-        for mois in range(1, 13):
-            interets = (capital * taux * (annee + mois / 12)) / 100
-            montant_total = capital + interets
-            data.append([f"{annee}-{mois:02d}", interets, montant_total])
+    debut_date = datetime(debut_annee, debut_mois, 1)
+    fin_date = datetime(fin_annee, fin_mois, 1)
+    while debut_date <= fin_date:
+        date_str = debut_date.strftime("%d-%m-%Y")
+        interets = (capital * taux * (debut_date.year + debut_date.month / 12)) / 100
+        montant_total = capital + interets
+        data.append([date_str, interets, montant_total])
+        debut_date = debut_date.replace(month=debut_date.month + 1) if debut_date.month < 12 else debut_date.replace(year=debut_date.year + 1, month=1)
     return data
 
-# Fonction pour calculer les intérêts composés sur une plage d'années
-def calculer_interets_composes(capital, taux, debut_annee, fin_annee):
+# Fonction pour calculer les intérêts composés sur une plage de temps
+def calculer_interets_composes(capital, taux, debut_annee, debut_mois, fin_annee, fin_mois):
     data = []
-    for annee in range(debut_annee, fin_annee + 1):
-        for mois in range(1, 13):
-            total_mois = annee + mois / 12
-            montant_total = capital * ((1 + (taux / 100)) ** total_mois)
-            interets = montant_total - capital
-            data.append([f"{annee}-{mois:02d}", interets, montant_total])
+    debut_date = datetime(debut_annee, debut_mois, 1)
+    fin_date = datetime(fin_annee, fin_mois, 1)
+    while debut_date <= fin_date:
+        date_str = debut_date.strftime("%d-%m-%Y")
+        total_mois = debut_date.year + debut_date.month / 12
+        montant_total = capital * ((1 + (taux / 100)) ** total_mois)
+        interets = montant_total - capital
+        data.append([date_str, interets, montant_total])
+        debut_date = debut_date.replace(month=debut_date.month + 1) if debut_date.month < 12 else debut_date.replace(year=debut_date.year + 1, month=1)
     return data
 
-# Fonction pour calculer les annuités pour un emprunt sur une plage d'années
-def calculer_annuite_emprunt(capital_emprunte, taux, debut_annee, fin_annee):
+# Fonction pour calculer les annuités pour un emprunt sur une plage de temps
+def calculer_annuite_emprunt(capital_emprunte, taux, debut_annee, debut_mois, fin_annee, fin_mois):
     data = []
-    for annee in range(debut_annee, fin_annee + 1):
-        for mois in range(1, 13):
-            duree = (fin_annee - debut_annee) * 12
-            taux_mensuel = taux / 12 / 100
-            annuite = capital_emprunte * (taux_mensuel / (1 - (1 + taux_mensuel) ** (-duree)))
-            data.append([f"{annee}-{mois:02d}", annuite])
+    debut_date = datetime(debut_annee, debut_mois, 1)
+    fin_date = datetime(fin_annee, fin_mois, 1)
+    while debut_date <= fin_date:
+        date_str = debut_date.strftime("%d-%m-%Y")
+        duree = (fin_date - debut_date).days / 30  # approximativement en mois
+        taux_mensuel = taux / 12 / 100
+        annuite = capital_emprunte * (taux_mensuel / (1 - (1 + taux_mensuel) ** (-duree)))
+        data.append([date_str, annuite])
+        debut_date = debut_date.replace(month=debut_date.month + 1) if debut_date.month < 12 else debut_date.replace(year=debut_date.year + 1, month=1)
     return data
 
-# Fonction pour calculer les annuités pour une obligation sur une plage d'années
-def calculer_annuite_obligation(capital_obligation, taux, debut_annee, fin_annee):
+# Fonction pour calculer les annuités pour une obligation sur une plage de temps
+def calculer_annuite_obligation(capital_obligation, taux, debut_annee, debut_mois, fin_annee, fin_mois):
     data = []
-    for annee in range(debut_annee, fin_annee + 1):
-        for mois in range(1, 13):
-            annuite = capital_obligation * (taux / 100)
-            data.append([f"{annee}-{mois:02d}", annuite])
+    debut_date = datetime(debut_annee, debut_mois, 1)
+    fin_date = datetime(fin_annee, fin_mois, 1)
+    while debut_date <= fin_date:
+        date_str = debut_date.strftime("%d-%m-%Y")
+        annuite = capital_obligation * (taux / 100)
+        data.append([date_str, annuite])
+        debut_date = debut_date.replace(month=debut_date.month + 1) if debut_date.month < 12 else debut_date.replace(year=debut_date.year + 1, month=1)
     return data
 
-# Fonction pour calculer les détails d'un escompte sur une plage d'années
-def calculer_escompte(capital, taux_escompte, debut_annee, fin_annee, taux_agios, tva):
+# Fonction pour calculer les détails d'un escompte sur une plage de temps
+def calculer_escompte(capital, taux_escompte, debut_annee, debut_mois, fin_annee, fin_mois, taux_agios, tva):
     data = []
-    for annee in range(debut_annee, fin_annee + 1):
-        for mois in range(1, 13):
-            escompte = capital * (taux_escompte * (annee + mois / 12)) / 360
-            montant_escompte = capital - escompte
+    debut_date = datetime(debut_annee, debut_mois, 1)
+    fin_date = datetime(fin_annee, fin_mois, 1)
+    while debut_date <= fin_date:
+        date_str = debut_date.strftime("%d-%m-%Y")
+        escompte = capital * (taux_escompte * (debut_date.year + debut_date.month / 12)) / 360
+        montant_escompte = capital - escompte
 
-            agios = montant_escompte * (taux_agios / 100)
-            montant_agios = montant_escompte + agios
+        agios = montant_escompte * (taux_agios / 100)
+        montant_agios = montant_escompte + agios
 
-            tva_sur_agios = agios * (tva / 100)
-            montant_total = montant_agios + tva_sur_agios
+        tva_sur_agios = agios * (tva / 100)
+        montant_total = montant_agios + tva_sur_agios
 
-            data.append([f"{annee}-{mois:02d}", escompte, montant_escompte, agios, montant_agios, tva_sur_agios, montant_total])
+        data.append([date_str, escompte, montant_escompte, agios, montant_agios, tva_sur_agios, montant_total])
+        debut_date = debut_date.replace(month=debut_date.month + 1) if debut_date.month < 12 else debut_date.replace(year=debut_date.year + 1, month=1)
     return data
 
 # Fonction pour sauvegarder les données dans un fichier
@@ -66,6 +82,15 @@ def sauvegarder_dans_fichier(nom_fichier, headers, data):
 # Fonction pour afficher un tableau
 def afficher_tableau(headers, data):
     print(tabulate(data, headers=headers, tablefmt="grid"))
+
+def collecter_date():
+    date_str = input("Entrez la date au format JJ-MM-AAAA : ")
+    try:
+        date = datetime.strptime(date_str, "%d-%m-%Y")
+        return date.year, date.month
+    except ValueError:
+        print("Format de date incorrect. Veuillez saisir la date au format JJ-MM-AAAA.")
+        return collecter_date()
 
 def main():
     print("Bienvenue dans le calculateur financier.")
@@ -79,60 +104,77 @@ def main():
                   "Choisissez l'option (1/2/3/4/5) : ")
 
     if choix == '1':
+        # Option 1: Intérêts simples pour un compte d'épargne
         capital = float(input("Entrez le capital initial sur le compte d'épargne : "))
         taux = float(input("Entrez le taux d'intérêt en pourcentage : "))
-        debut_annee = int(input("Entrez l'année de début : "))
-        fin_annee = int(input("Entrez l'année de fin : "))
 
-        data = calculer_interets_simples(capital, taux, debut_annee, fin_annee)
+        print("Saisie de la plage de temps pour le calcul :")
+        debut_annee, debut_mois = collecter_date()
+        fin_annee, fin_mois = collecter_date()
+
+        data = calculer_interets_simples(capital, taux, debut_annee, debut_mois, fin_annee, fin_mois)
         headers = ["Date", "Intérêts", "Montant Total"]
         afficher_tableau(headers, data)
         sauvegarder_dans_fichier("interets_simples.txt", headers, data)
 
     elif choix == '2':
+        # Option 2: Intérêts composés pour un escompte
         capital = float(input("Entrez le montant du bordereau d'escompte : "))
         taux_escompte = float(input("Entrez le taux d'escompte en pourcentage : "))
-        debut_annee = int(input("Entrez l'année de début : "))
-        fin_annee = int(input("Entrez l'année de fin : "))
+
+        print("Saisie de la plage de temps pour le calcul :")
+        debut_annee, debut_mois = collecter_date()
+        fin_annee, fin_mois = collecter_date()
+
         taux_agios = float(input("Entrez le taux d'agios en pourcentage : "))
         tva = float(input("Entrez le taux de TVA en pourcentage : "))
 
-        data = calculer_escompte(capital, taux_escompte, debut_annee, fin_annee, taux_agios, tva)
+        data = calculer_escompte(capital, taux_escompte, debut_annee, debut_mois, fin_annee, fin_mois, taux_agios, tva)
         headers = ["Date", "Escompte", "Montant après Escompte", "Agios", "Montant Total avec Agios", "TVA sur les Agios", "Montant Total avec TVA"]
         afficher_tableau(headers, data)
         sauvegarder_dans_fichier("bordereau_escompte.txt", headers, data)
 
     elif choix == '3':
+        # Option 3: Annuités pour un emprunt
         capital_emprunte = float(input("Entrez le montant emprunté : "))
         taux = float(input("Entrez le taux d'intérêt annuel en pourcentage : "))
-        debut_annee = int(input("Entrez l'année de début : "))
-        fin_annee = int(input("Entrez l'année de fin : "))
 
-        data = calculer_annuite_emprunt(capital_emprunte, taux, debut_annee, fin_annee)
+        print("Saisie de la plage de temps pour le calcul :")
+        debut_annee, debut_mois = collecter_date()
+        fin_annee, fin_mois = collecter_date()
+
+        data = calculer_annuite_emprunt(capital_emprunte, taux, debut_annee, debut_mois, fin_annee, fin_mois)
         headers = ["Date", "Annuité"]
         afficher_tableau(headers, data)
         sauvegarder_dans_fichier("annuite_emprunt.txt", headers, data)
 
     elif choix == '4':
+        # Option 4: Annuités pour une obligation
         capital_obligation = float(input("Entrez le montant de l'obligation : "))
         taux = float(input("Entrez le taux d'intérêt annuel en pourcentage : "))
-        debut_annee = int(input("Entrez l'année de début : "))
-        fin_annee = int(input("Entrez l'année de fin : "))
 
-        data = calculer_annuite_obligation(capital_obligation, taux, debut_annee, fin_annee)
+        print("Saisie de la plage de temps pour le calcul :")
+        debut_annee, debut_mois = collecter_date()
+        fin_annee, fin_mois = collecter_date()
+
+        data = calculer_annuite_obligation(capital_obligation, taux, debut_annee, debut_mois, fin_annee, fin_mois)
         headers = ["Date", "Annuité"]
         afficher_tableau(headers, data)
         sauvegarder_dans_fichier("annuite_obligation.txt", headers, data)
 
     elif choix == '5':
+        # Option 5: Escompte (bordereau d'escompte)
         capital = float(input("Entrez le montant de la facture : "))
         taux_escompte = float(input("Entrez le taux d'escompte en pourcentage : "))
-        debut_annee = int(input("Entrez l'année de début : "))
-        fin_annee = int(input("Entrez l'année de fin : "))
+
+        print("Saisie de la plage de temps pour le calcul :")
+        debut_annee, debut_mois = collecter_date()
+        fin_annee, fin_mois = collecter_date()
+
         taux_agios = float(input("Entrez le taux d'agios en pourcentage : "))
         tva = float(input("Entrez le taux de TVA en pourcentage : "))
 
-        data = calculer_escompte(capital, taux_escompte, debut_annee, fin_annee, taux_agios, tva)
+        data = calculer_escompte(capital, taux_escompte, debut_annee, debut_mois, fin_annee, fin_mois, taux_agios, tva)
         headers = ["Date", "Escompte", "Montant après Escompte", "Agios", "Montant Total avec Agios", "TVA sur les Agios", "Montant Total avec TVA"]
         afficher_tableau(headers, data)
         sauvegarder_dans_fichier("bordereau_escompte.txt", headers, data)
